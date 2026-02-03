46b87e591031c8bae91844b632689bdeaf89e4b8
Stefano-ChiadoPiat <stefanochiadopiat@gmail.com>
2026-02-03 17:05:30 +0100

    Creazione e Fix del setup_linux

--- progetto-windows/backend/main.py
+++ progetto-windows/backend/main.py
@@ -5,8 +5,7 @@ import random;
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware, 
-                   allow_origins=["http://localhost:5173"], 
-                   allow_credentials=True, 
+                   allow_origins=["*"],
                    allow_methods=["*"], 
                    allow_headers=["*"]
                    )
---
+++ progetto-windows/setup_linux.sh
@@ -0,0 +1,20 @@
+echo Setup iniziale Linux x64
+
+echo Installazione NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+exec bash
+nvm install node
+
+echo Eliminazione, se presente, del Virtual Enviorment
+if [ -d "nome_cartella" ]; then
+  rm -r venv
+fi
+
+echo Creazione del Virtual Enviorment
+python3 -m venv venv
+source venv/bin/activate
+
+echo Installazione Librerie
+pip install fastapi uvicorn
+
+read -p "Finito. Premi INVIO per uscire!"
